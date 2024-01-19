303117d1e939123d0ec3be02ca074e4eb09cc643
Жиганов Сергей  Владимирович <sergey.zhiganov@eltex.loc>
2024-01-19 20:35:08 +0700

    crc for AirCondition-1

--- src/crc/CrcTestForIr.java
+++ src/crc/CrcTestForIr.java
@@ -3,6 +3,7 @@ package crc;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.*;
 import java.util.zip.Adler32;
 import java.util.zip.CRC32;
 
@@ -13,10 +14,12 @@ import java.util.zip.CRC32;
  * <BR/> Необходимо олпределть алгоритм вычисления.
  * <BR/>
  * <BR/>
- * - A=0x55600600c4012a00,B=0x1c0
+ * - A=0x55600600c4012a00,B=0x1c0      -- 1C0 = 448 =  1 1100 0000
  * - A=0x55600600c4013a00,B=0x1e0
  * - A=0x55a00600c4012a00,B=0xc0
  * - A=0x550006000a812a00, B=0x1e8
+ *
+ * 227 = E3 = 1110 0011
  * <BR/>
  * <BR/> User: svj
  * <BR/> Date: 28.09.23 19:49
@@ -36,27 +39,69 @@ public class CrcTestForIr {
         //short[] data = new short[] {0x55, 0x60, 0x06, 0x00, 0xC4, 0x02, 0x2a, 0x00 };  // 196
         // 0xC4 = 196
         // в байти С4 - это отрицательное число
-        byte[] dataByte = new byte[] {0x55, 0x60, 0x06, 0x00, (byte)0xC4, 0x02, 0x2a, 0x00 };  // C4 - 196
+        byte[] dataByte = new byte[] {0x55, 0x60, 0x06, 0x00, (byte)0xC4, 0x01, 0x2a, 0x00 };  // C4 - 196
         int[] dataInt = new int[] {0x55, 0x60, 0x06, 0x00, 0xC4, 0x02, 0x2a, 0x00 };  // C4 - 196
-        // todo long - точно знак пропадет
-        long[] dataLong = new long[] {0x55, 0x60, 0x06, 0x00, 0xC4, 0x02, 0x2a, 0x00 };  // C4 - 196
+        // long - точно знак пропадет
 
         // 55 00 06 00 0a 81 2a 00
         byte[] dataByte4 = new byte[] {0x55, 0x00, 0x06, 0x00, 0x0a, (byte)0x81, 0x2a, 0x00 };  // 81 -
 
-        checkByte();
+
+        Map<String,long[]> dataList = new HashMap<>();
+        long[] dataLong = new long[] {0x55, 0x60, 0x06, 0x00, 0xC4, 0x01, 0x2a, 0x00 };  // 1C0 = 448
+        dataList.put("448", dataLong);
+        // 55 60 06 00 c4 01 3a 00,B=0x1e0   = 480
+        dataLong = new long[] {0x55, 0x60, 0x06, 0x00, 0xC4, 0x01, 0x3a, 0x00 };  // 1E0 = 480
+        dataList.put("480", dataLong);
+        // 55 a0 06 00 c4 01 2a 00,  B=0xc0   = 192
+        dataLong = new long[] {0x55, 0xA0, 0x06, 0x00, 0xC4, 0x01, 0x2a, 0x00 };  // C0 = 192
+        dataList.put("192", dataLong);
+        // 55 00 06 00 0a 81 2a 00, B=0x1e8  = 488
+        dataLong = new long[] {0x55, 0x00, 0x06, 0x00, 0x0A, 0x81, 0x2a, 0x00 };  // 1E8 = 488
+        dataList.put("488", dataLong);
+
+        // 55 60 06 00 c4 02 2a 00, B=0x1c6  = 454
+        dataLong = new long[] {0x55, 0x60, 0x06, 0x00, 0xC4, 0x02, 0x2a, 0x00 };  // 1C6 = 454
+        dataList.put("454", dataLong);
+        // 55 20 06 00 c4 01 2a 00, B=0x140  = 320
+        dataLong = new long[] {0x55, 0x20, 0x06, 0x00, 0xC4, 0x01, 0x2a, 0x00 };  // 140 = 320
+        dataList.put("320", dataLong);
+        // 55 e0 06 00 c4 01 2a 00, B=0x20  = 32
+        dataLong = new long[] {0x55, 0xE0, 0x06, 0x00, 0xC4, 0x01, 0x2a, 0x00 };
+        dataList.put("32", dataLong);
+        // 55 10 06 00 c4 01 2a 00, B=0x120  = 288
+        dataLong = new long[] {0x55, 0x10, 0x06, 0x00, 0xC4, 0x01, 0x2a, 0x00 };
+        dataList.put("288", dataLong);
+        // 55 90 06 00 c4 01 2a 00, B=0xa0  = 160
+        dataLong = new long[] {0x55, 0x90, 0x06, 0x00, 0xC4, 0x01, 0x2a, 0x00 };
+        dataList.put("160", dataLong);
+        // 55 10 06 00 c4 01 3a 00, B=0x110  = 272
+        dataLong = new long[] {0x55, 0x10, 0x06, 0x00, 0xC4, 0x01, 0x3a, 0x00 };
+        dataList.put("272", dataLong);
+
+        long result;
+        int ic = 1;
+        for (Map.Entry<String,long[]> entry : dataList.entrySet()) {
+            result = handler.createSumNibe(entry.getValue());
+            System.out.println(ic + ") result = " + result + "; crc = " + entry.getKey());
+            ic++;
+        }
+
+
+    //    checkByte();
 
         //short[] result = handler.create16(data);
         //short[] result = handler.create32(data);
         //short result = handler.createSum4(dataInt);
-        long result = handler.createSumLong1(dataLong);
+        //long result = handler.createSumLong1(dataLong);
+    //    long result = handler.createSumNibe(dataLong);   // 227 = E3
 
         // исходник
         //printSourceByte(dataByte);
-        printSource(dataLong);
+    //    printSource(dataLong);
 
         //System.out.println("result = " + DumpTools.printArray(result));
-        System.out.println("result = " + result);
+        //System.out.println("result = " + result);
 
     }
 
@@ -219,6 +264,37 @@ public class CrcTestForIr {
           checksum ^= data[i];
         }
         return checksum;
+    }
+
+    private long createSumNibe(long[] data) {
+
+        long checksum = 0;
+        for (int i = 0; i < 8; i++) {
+            if (i == 7)
+                checksum += reverseBits8(data[i], 2);  // Byte 7 only has 2 bits
+            else
+                checksum += reverseBits8(data[i], 8);
+        }
+        long result = reverseBits8(checksum, 8);
+        result = result << 1;
+
+        return result;
+    }
+
+    // Reverses bit order for a uint8_t.
+    // Can modify the bitlength that needs to be reversed (needed 8, 5 bit or 2 bit reverse)
+    private long reverseBits8(long value, int bitLength) {
+        long reversedValue = 0;
+
+        for (int i = 0; i < bitLength; i++) {
+            // Extract the i-th bit from the original value
+            long bit = (value >> i) & 1;
+
+            // Set the (bitLength - 1 - i)-th bit in the reversed value
+            reversedValue |= bit << (bitLength - 1 - i);
+        }
+
+        return reversedValue;
     }
 
     /**
